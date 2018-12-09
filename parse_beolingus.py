bcaec4de0a0daffe7815aa44e3c6d5cc7e27fecc
Jan <JBigalke@smail.uni-koeln.de>
2018-12-09 13:51:33 +0100

    assignment6

--- parse_beolingus.py
+++ parse_beolingus.py
@@ -1,65 +1,62 @@
-import pickle
+import re
+import unicodedata
+import input_output
 
-def read_beolingus(file):
+
+def beolingus_as_list(file):
     lines = []
-    with open (file, 'rt') as file:
-        for line in file:
+    with open(file, mode='r') as f:
+        for line in f:
             line = line.rstrip()
             if not line.startswith('#'):
                 lines.append(line)
     return lines
 
-lines = read_beolingus("de-en.txt")
+
+def split_beolingus(lines):
+    beo_dict = {}
+    for i, line in enumerate(lines):
+        i += 1
+        line_dict = {}
+        line = line.split('::')
+        german = line[0]
+        english = line[1]
+        german = german.split('|')
+        english = english.split('|')
+        # if len(german) != len(english):
+        #    print(line)
+        for e, l in enumerate(german):
+            line_dict[german[e]] = english[e]
+        beo_dict[i] = line_dict
+    return beo_dict
 
 
-
-def split_into_german_english(line):
-    splitted = line.split('::')
-    return splitted
-
-def split_parts(line):
-    german = line[0]
-    english = line[1]
-    germanparts = german.split('|')
-    englischparts = english.split('|')
-    newParts = {}
-    for i, germanpart in enumerate(germanparts):
-        newPart = {germanparts[i] : englischparts[i]}
-        newParts.update(newPart)
-    return newParts
+def get_usg(beo_as_dict):
+    usg_set = set()
+    usg_pattern = re.compile(r'\[\w+\.?\]')
+    counter = 0
+    for k, v in beo_as_dict.items():
+        # if counter < 10:
+        usg_matches = usg_pattern.findall(str(v))
+        for match in usg_matches:
+            usg_set.add(match)
+        counter += 1
+    return usg_set
 
 
-def save_dict(dictonary):
-    filename = "newLex"
-    with open(filename, 'wb') as picklefile:
-        pickle.dump(dictonary, picklefile)
-    picklefile.close()
-
-def load_dict():
-    new_dict = {}
-    with open("newLex", 'rb') as picklefile:
-        new_dict = pickle.load(picklefile)
-    picklefile.close()
-    return new_dict;
-
-
-dictonary = {}
+def get_pos(beo_as_dict):
+    pos_set = set()
+    pos_pattern = re.compile(r'\{\w+\.?\}')
+    counter = 0
+    for k, v in beo_as_dict.items():
+        # if counter < 10:
+        usg_matches = pos_pattern.findall(str(v))
+        for match in usg_matches:
+            pos_set.add(match)
+        counter += 1
+    return pos_set
 
 
-for i, line in enumerate(lines):
-    GermanEnglish = split_into_german_english(line)
-    NewParts = split_parts(GermanEnglish)
-    part = {i+1 : NewParts}
-    dictonary.update(part)
-
-save_dict(dictonary)
-
-new_dict = load_dict()
-
-
-
-
-
-
-for k, v in new_dict.items():
-    print(k, v)
+pos = get_pos(input_output.deserialize('newLex'))
+for e in pos:
+    print(e)
